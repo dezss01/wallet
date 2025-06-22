eefeb25608abfc81586616aa6ae209880d9bcfc2
dezss01 <aka.dez@yandex.ru>
2025-06-22 20:36:56 +0300

    fix eslintrc

---
+++ frontend/base_front/.eslintrc.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  }
+}
